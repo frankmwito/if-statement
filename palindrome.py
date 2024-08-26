ed7078813bab6ff4daeb93e5aae2603fa0c9fa97
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-26 15:37:54 +0300

    string palindrome check

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,12 @@
 # Check if a given string is a palindrome
+
+word = input("Enter any word: ")
+
+# Reverse the word
+reversed_word = word[::-1]
+
+# Check if the original word is equal to the reversed word
+if word == reversed_word:
+    print(f"Your word '{word}' is a palindrome.")
+else:
+    print(f"Your word '{word}' is not a palindrome.")
