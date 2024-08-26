96b843f8616e748bc68768bcf9db7b1c65ed7fdf
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-26 15:13:25 +0300

    Greatest number in a list of three

--- greatestNumber.py
+++ greatestNumber.py
@@ -1,1 +1,18 @@
-# return the greatest number from a list of three numbers
+# List to store the numbers
+numbers = list()
+
+# Get three numbers from the user
+for i in range(3):
+    numbers.append(int(input(f"Enter any number {i+1}: ")))
+    
+print("Your list is:", numbers)
+
+# Determine the greatest number
+if numbers[0] >= numbers[1] and numbers[0] >= numbers[2]:
+    greatest = numbers[0]
+elif numbers[1] >= numbers[0] and numbers[1] >= numbers[2]:
+    greatest = numbers[1]
+else:
+    greatest = numbers[2]
+
+print(f"The greatest number is: {greatest}")
